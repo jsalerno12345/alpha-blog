57306d698b1ea28a1649e3d0c3de1a82f024ffea
jsalerno12345 <jsalerno12345@gmail.com>
2024-09-25 11:17:34 -0400

    adding article listing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
     def show 
         @article =  Article.find(params[:id]) #turns into an instance variable
     end
+
+    def index
+        @articles =  Article.all
+    end
 end 
